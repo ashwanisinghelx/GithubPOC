ec5bd39af0174ad0be6da0b26395890ec900cd45
Ashwani Sinhg <singh111ashwani@gmail.com>
2026-03-27 23:33:26 +0530

    added table of 3 AB#3

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def main():
     print("Hello from githubpoc!")
     num = 2
     for i in range(1,11):
-        print(i * num)
-
+        print(i * num, end = '->')
+    num = 3
+    print()
+    for i in range(1,11):
+        print(i * num, end = '->')
+    
 
 if __name__ == "__main__":
     main()
